6efa8c786cb6f770addf67a1973f45418e050168
Carol Muñoz <clmunoz21@misena.edu.co>
2021-12-08 20:25:06 -0500

    .add login actualizado

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,14 +1,18 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+@extends('layouts.main', ['class' => 'off-canvas-sidebar', 'activePage' => 'home', 'title' => __('Material Dashboard')])
 
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-        </div>
-
-        @if (session('status'))
+@section('content')
+<div class="container" style="height: auto;">
+  <div class="row justify-content-center">
+      <div class="col-lg-7 col-md-8">
+          <div class="card card-login card-hidden mb-3">
+            <div class="card-header card-header-primary text-center">
+              <p class="card-title"><strong><p>¿Olvidaste tu contraseña? No hay problema. <br>
+              Simplemente háganos saber su dirección de correo electrónico y le enviaremos un enlace <br> para restablecer la contraseña</p></strong></p>
+            </div>
+            <div class="card-body">
+              <p class="card-description text-center"></p>
+              <p>
+              @if (session('status'))
             <div class="mb-4 font-medium text-sm text-green-600">
                 {{ session('status') }}
             </div>
@@ -19,16 +23,34 @@
         <form method="POST" action="{{ route('password.email') }}">
             @csrf
 
-            <div class="block">
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            <div class="bmd-form-group{{ $errors->has('email') ? ' has-danger' : '' }} mt-3">
+              <div class="input-group">
+                <div class="input-group-prepend">
+                  <span class="input-group-text">
+                    <i class="material-icons">email</i>
+                  </span>
+                </div>
+                <input type="email" name="email" class="form-control" placeholder="{{ __('Email...') }}" value="{{ old('email') }}" required>
+              </div>
+              @if ($errors->has('email'))
+                <div id="email-error" class="error text-danger pl-3" for="email" style="display: block;">
+                  <strong>{{ $errors->first('email') }}</strong>
+                </div>
+              @endif
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                <x-jet-button>
-                    {{ __('Email Password Reset Link') }}
-                </x-jet-button>
+            <button type="submit" class="btn btn-primary btn-link btn-lg">{{ __('Restablecer') }}</button>
+            </div>
+            <div class="flex items-center justify-end mt-3">
+            <button type="submit" class="btn btn-primary btn-link btn-lg">{{ __('Solicitar vinculo nuevo') }}</button>.
             </div>
         </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+              </p>
+              </p>
+            </div>
+          </div>
+      </div>
+  </div>
+</div>
+@endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,106 +1,250 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+@extends('layouts.main', ['class' => 'off-canvas-sidebar', 'activePage' => 'register', 'title' => __('Material Dashboard')])
 
-        <x-jet-validation-errors class="mb-4" />
+@section('content')
+<div class="container" style="height: auto;">
+  <div class="row align-items-center">
+    <div class="col-lg-4 col-md-6 col-sm-8 ml-auto mr-auto">
+      <form class="form" method="POST" action="{{ route('register') }}">
+        @csrf
 
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
-
-            <div>
-                <x-jet-label for="name" value="{{ __('Rol') }}" />
-                <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+        <div class="card card-login card-hidden mb-3">
+          <div class="card-header card-header-primary text-center">
+            <h4 class="card-title"><strong>{{ __('Register') }}</strong></h4>
+            <div class="social-line">
+              <a href="#pablo" class="btn btn-just-icon btn-link btn-white">
+                <i class="fa fa-facebook-square"></i>
+              </a>
+              <a href="#pablo" class="btn btn-just-icon btn-link btn-white">
+                <i class="fa fa-twitter"></i>
+              </a>
+              <a href="#pablo" class="btn btn-just-icon btn-link btn-white">
+                <i class="fa fa-google-plus"></i>
+              </a>
             </div>
-
-            <div>
-                <x-jet-label for="document_type" value="{{ __('Tipo de Documento') }}" />
-                <x-jet-input id="document_type" class="block mt-1 w-full" type="text" name="document_type" :value="old('document_type')" required autofocus autocomplete="document_type" />
-            </div>
-            <div>
-                <x-jet-label for="document_number" value="{{ __('Numero de Documento') }}" />
-                <x-jet-input id="document_number" class="block mt-1 w-full" type="text" name="document_number" :value="old('document_number')" required autofocus autocomplete="document_number" />
-            </div>
-
-            <div>
-                <x-jet-label for="full_name" value="{{ __('Nombre Completo') }}" />
-                <x-jet-input id="full_name" class="block mt-1 w-full" type="text" name="full_name" :value="old('full_name')" required autofocus autocomplete="full_name" />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="city" value="{{ __('Ciudad') }}" />
-                <x-jet-input id="city" class="block mt-1 w-full" type="text" name="city" :value="old('city')" required />
-            </div>
-            <div class="mt-4">
-                <x-jet-label for="state" value="{{ __('Departamento') }}" />
-                <x-jet-input id="state" class="block mt-1 w-full" type="text" name="state" :value="old('state')" required />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="address" value="{{ __('Dirección') }}" />
-                <x-jet-input id="address" class="block mt-1 w-full" type="text" name="address" :value="old('address')" required />
-            </div>
-
-
-            <div class="mt-4">
-                <x-jet-label for="age" value="{{ __('Edad') }}" />
-                <x-jet-input id="age" class="block mt-1 w-full" type="text" name="age" :value="old('age')" required />
-            </div>
-
-
-            <div class="mt-4">
-                <x-jet-label for="gender" value="{{ __('Genero') }}" />
-                <x-jet-input id="gender" class="block mt-1 w-full" type="text" name="gender" :value="old('gender')" required />
-            </div>
-
-
-            <div class="mt-4">
-                <x-jet-label for="phone" value="{{ __('Telefono') }}" />
-                <x-jet-input id="phone" class="block mt-1 w-full" type="text" name="phone" :value="old('phone')" required />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="email" value="{{ __('Correo Electronico') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Contraseña') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="password_confirmation" value="{{ __('Confirmación de Contraseña') }}" />
-                <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
-            </div>
-
-            @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
-                <div class="mt-4">
-                    <x-jet-label for="terms">
-                        <div class="flex items-center">
-                            <x-jet-checkbox name="terms" id="terms"/>
-
-                            <div class="ml-2">
-                                {!! __('I agree to the :terms_of_service and :privacy_policy', [
-                                        'terms_of_service' => '<a target="_blank" href="'.route('terms.show').'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Terms of Service').'</a>',
-                                        'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Privacy Policy').'</a>',
-                                ]) !!}
-                            </div>
-                        </div>
-                    </x-jet-label>
+          </div>
+          <!--Name-->
+          <div class="card-body ">
+            <p class="card-description text-center">{{ __('Or Be Classical') }}</p>
+            <div class="bmd-form-group{{ $errors->has('name') ? ' has-danger' : '' }}">
+              <div class="input-group">
+                <div class="input-group-prepend">
+                  <span class="input-group-text">
+                      <i class="material-icons">face</i>
+                  </span>
                 </div>
-            @endif
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-jet-button class="ml-4">
-                    {{ __('Register') }}
-                </x-jet-button>
+                <input type="text" name="name" class="form-control" placeholder="{{ __('Rol...') }}" value="{{ old('name') }}" required autocomplete="name">
+              </div>
+              @if ($errors->has('name'))
+                <div id="name-error" class="error text-danger pl-3" for="name" style="display: block;">
+                  <strong>{{ $errors->first('name') }}</strong>
+                </div>
+              @endif
             </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+            <!--Type_Document-->
+            <div class="bmd-form-group{{ $errors->has('document_type') ? ' has-danger' : '' }} mt-3">
+              <div class="input-group">
+                <div class="input-group-prepend">
+                  <span class="input-group-text">
+                    <i class="material-icons">face</i>
+                  </span>
+                </div>
+                <input type="text" name="document_type" class="form-control" placeholder="{{ __('Tipo de Documento...') }}" value="{{ old('document_type') }}" required autocomplete="document_type">
+              </div>
+              @if ($errors->has('document_type'))
+                <div id="document_type-error" class="error text-danger pl-3" for="document_type" style="display: block;">
+                  <strong>{{ $errors->first('document_type') }}</strong>
+                </div>
+              @endif
+            </div>
+            <!--Document_number-->
+            <div class="bmd-form-group{{ $errors->has('document_number') ? ' has-danger' : '' }} mt-3">
+              <div class="input-group">
+                <div class="input-group-prepend">
+                  <span class="input-group-text">
+                    <i class="material-icons">face</i>
+                  </span>
+                </div>
+                <input type="text" name="document_number" class="form-control" placeholder="{{ __('Numero de Documento...') }}" value="{{ old('document_number') }}" required autocomplete="document_number">
+              </div>
+              @if ($errors->has('document_number'))
+                <div id="document_number-error" class="error text-danger pl-3" for="document_number" style="display: block;">
+                  <strong>{{ $errors->first('document_number') }}</strong>
+                </div>
+              @endif
+            </div>
+              <!--full_name-->
+              <div class="bmd-form-group{{ $errors->has('full_name') ? ' has-danger' : '' }} mt-3">
+              <div class="input-group">
+                <div class="input-group-prepend">
+                  <span class="input-group-text">
+                    <i class="material-icons">face</i>
+                  </span>
+                </div>
+                <input type="text" name="full_name" class="form-control" placeholder="{{ __('Nombre Completo...') }}" value="{{ old('full_name') }}" required autocomplete="full_name">
+              </div>
+              @if ($errors->has('full_name'))
+                <div id="full_name-error" class="error text-danger pl-3" for="full_name" style="display: block;">
+                  <strong>{{ $errors->first('full_name') }}</strong>
+                </div>
+              @endif
+            </div>
+              <!--city-->
+              <div class="bmd-form-group{{ $errors->has('city') ? ' has-danger' : '' }} mt-3">
+              <div class="input-group">
+                <div class="input-group-prepend">
+                  <span class="input-group-text">
+                    <i class="material-icons">email</i>
+                  </span>
+                </div>
+                <input type="text" name="city" class="form-control" placeholder="{{ __('Ciudad...') }}" value="{{ old('city') }}" required autocomplete="city">
+              </div>
+              @if ($errors->has('city'))
+                <div id="city-error" class="error text-danger pl-3" for="city" style="display: block;">
+                  <strong>{{ $errors->first('city') }}</strong>
+                </div>
+              @endif
+            </div>
+              <!--state-->
+              <div class="bmd-form-group{{ $errors->has('state') ? ' has-danger' : '' }} mt-3">
+              <div class="input-group">
+                <div class="input-group-prepend">
+                  <span class="input-group-text">
+                    <i class="material-icons">email</i>
+                  </span>
+                </div>
+                <input type="text" name="state" class="form-control" placeholder="{{ __('Departamento...') }}" value="{{ old('state') }}" required autocomplete="state">
+              </div>
+              @if ($errors->has('city'))
+                <div id="state-error" class="error text-danger pl-3" for="state" style="display: block;">
+                  <strong>{{ $errors->first('state') }}</strong>
+                </div>
+              @endif
+            </div>
+              <!--address-->
+              <div class="bmd-form-group{{ $errors->has('address') ? ' has-danger' : '' }} mt-3">
+              <div class="input-group">
+                <div class="input-group-prepend">
+                  <span class="input-group-text">
+                    <i class="material-icons">email</i>
+                  </span>
+                </div>
+                <input type="text" name="address" class="form-control" placeholder="{{ __('Dirección...') }}" value="{{ old('address') }}" required autocomplete="address">
+              </div>
+              @if ($errors->has('address'))
+                <div id="address-error" class="error text-danger pl-3" for="address" style="display: block;">
+                  <strong>{{ $errors->first('address') }}</strong>
+                </div>
+              @endif
+            </div>
+              <!--age-->
+              <div class="bmd-form-group{{ $errors->has('age') ? ' has-danger' : '' }} mt-3">
+              <div class="input-group">
+                <div class="input-group-prepend">
+                  <span class="input-group-text">
+                    <i class="material-icons">email</i>
+                  </span>
+                </div>
+                <input type="text" name="age" class="form-control" placeholder="{{ __('Edad...') }}" value="{{ old('age') }}" required autocomplete="age">
+              </div>
+              @if ($errors->has('age'))
+                <div id="age-error" class="error text-danger pl-3" for="age" style="display: block;">
+                  <strong>{{ $errors->first('age') }}</strong>
+                </div>
+              @endif
+            </div>
+              <!--gender-->
+              <div class="bmd-form-group{{ $errors->has('gender') ? ' has-danger' : '' }} mt-3">
+              <div class="input-group">
+                <div class="input-group-prepend">
+                  <span class="input-group-text">
+                    <i class="material-icons">email</i>
+                  </span>
+                </div>
+                <input type="text" name="gender" class="form-control" placeholder="{{ __('Genero...') }}" value="{{ old('gender') }}" required autocomplete="gender">
+              </div>
+              @if ($errors->has('gender'))
+                <div id="gender-error" class="error text-danger pl-3" for="gender" style="display: block;">
+                  <strong>{{ $errors->first('gender') }}</strong>
+                </div>
+              @endif
+            </div>
+              <!--phone-->
+              <div class="bmd-form-group{{ $errors->has('phone') ? ' has-danger' : '' }} mt-3">
+              <div class="input-group">
+                <div class="input-group-prepend">
+                  <span class="input-group-text">
+                    <i class="material-icons">email</i>
+                  </span>
+                </div>
+                <input type="text" name="phone" class="form-control" placeholder="{{ __('Telefono...') }}" value="{{ old('phone') }}" required autocomplete="phone">
+              </div>
+              @if ($errors->has('phone'))
+                <div id="phone-error" class="error text-danger pl-3" for="phone" style="display: block;">
+                  <strong>{{ $errors->first('phone') }}</strong>
+                </div>
+              @endif
+            </div>
+            <div class="bmd-form-group{{ $errors->has('email') ? ' has-danger' : '' }} mt-3">
+              <div class="input-group">
+                <div class="input-group-prepend">
+                  <span class="input-group-text">
+                    <i class="material-icons">email</i>
+                  </span>
+                </div>
+                <input type="email" name="email" class="form-control" placeholder="{{ __('Email...') }}" value="{{ old('email') }}" required>
+              </div>
+              @if ($errors->has('email'))
+                <div id="email-error" class="error text-danger pl-3" for="email" style="display: block;">
+                  <strong>{{ $errors->first('email') }}</strong>
+                </div>
+              @endif
+            </div>
+            <div class="bmd-form-group{{ $errors->has('password') ? ' has-danger' : '' }} mt-3">
+              <div class="input-group">
+                <div class="input-group-prepend">
+                  <span class="input-group-text">
+                    <i class="material-icons">lock_outline</i>
+                  </span>
+                </div>
+                <input type="password" name="password" id="password" class="form-control" placeholder="{{ __('Password...') }}" required>
+              </div>
+              @if ($errors->has('password'))
+                <div id="password-error" class="error text-danger pl-3" for="password" style="display: block;">
+                  <strong>{{ $errors->first('password') }}</strong>
+                </div>
+              @endif
+            </div>
+            <div class="bmd-form-group{{ $errors->has('password_confirmation') ? ' has-danger' : '' }} mt-3">
+              <div class="input-group">
+                <div class="input-group-prepend">
+                  <span class="input-group-text">
+                    <i class="material-icons">lock_outline</i>
+                  </span>
+                </div>
+                <input type="password" name="password_confirmation" id="password_confirmation" class="form-control" placeholder="{{ __('Confirm Password...') }}" required>
+              </div>
+              @if ($errors->has('password_confirmation'))
+                <div id="password_confirmation-error" class="error text-danger pl-3" for="password_confirmation" style="display: block;">
+                  <strong>{{ $errors->first('password_confirmation') }}</strong>
+                </div>
+              @endif
+            </div>
+            <div class="form-check mr-auto ml-3 mt-3">
+              <label class="form-check-label">
+                <input class="form-check-input" type="checkbox" id="policy" name="policy" {{ old('policy', 1) ? 'checked' : '' }} >
+                <span class="form-check-sign">
+                  <span class="check"></span>
+                </span>
+                {{ __('I agree with the ') }} <a href="#">{{ __('Privacy Policy') }}</a>
+              </label>
+            </div>
+          </div>
+          <div class="card-footer justify-content-center">
+            <button type="submit" class="btn btn-primary btn-link btn-lg">{{ __('Create account') }}</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+@endsection
